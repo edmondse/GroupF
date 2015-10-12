dc24b73c6c7314ecf70d2bafcb4a54e15a2c898b
edmondse <edmondse@uni.coventry.ac.uk>
2015-10-12 09:34:44 +0100

    Update macadd.py

--- macadd.py
+++ macadd.py
@@ -1,6 +1,6 @@
 # The following programming accepts a partial or complete MAC
 # address from the user and then returns the vendor back.
-# Written by Group F (FSociety), 11-Oct-15, Python v3.4.3, version 1.0
+# Written by Group F (FSoc), 11-Oct-15, Python v3.4.3, version 1.0
 
 # I CAN'T FIGURE OUT HOW TO DO THE WHOLE POST/GET THING,
 # HERE'S THE WORLD'S LAZIEST SOLUTION TO THIS PROBLEM.
